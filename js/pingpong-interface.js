9f6b5d0fb670d03989f87cd9c998578171410d52
kelvinmunene <karvinmitchar@yahoo.com>
2017-05-24 12:14:45 +0300

    advanced pingpong

---
+++ js/pingpong-interface.js
@@ -0,0 +1,12 @@
+var Calculator1 = require('./../js/pingpong.js').calculatorModule;
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var simpleCalculator = new Calculator1("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
